a0c0672c9501b4343a8a346976ccf1dea2aa0fdc
Darth Vader <vader.pepe@email.com>
2025-07-13 16:05:03 +0700

    feat: socket.io [WIP]

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,14 +3,27 @@ import express, { type Express } from "express";
 import helmet from "helmet";
 import { pino } from "pino";
 import { appendFile } from "fs";
+import { type proto } from "baileys";
 
 import { healthCheckRouter } from "@/api/healthCheck/healthCheckRouter";
 import { userRouter } from "@/api/user/userRouter";
 import { openAPIRouter } from "@/api-docs/openAPIRouter";
 import errorHandler from "@/common/middleware/errorHandler";
-import rateLimiter from "@/common/middleware/rateLimiter";
 import requestLogger from "@/common/middleware/requestLogger";
 import { env } from "@/common/utils/envConfig";
+
+
+
+export type Payload = {
+  event: "application.startup" | "call" | "chats.delete" | "chats.set" | "chats.update" | "chats.upsert" | "connection.update" | "contacts.set" | "contacts.update" | "contacts.upsert" | "group-participants.update" | "group.update" | "groups.upsert" | "labels.association" | "labels.edit" | "logout.instance" | "messages.delete" | "messages.set" | "messages.update" | "messages.upsert" | "presence.update" | "qrcode.updated" | "remove.instance" | "send.message"
+  instance: string
+  data: proto.IWebMessageInfo
+  destination: string
+  date_time: string
+  sender: string
+  server_url: string
+  apikey: string
+};
 
 const logger = pino({ name: "server start" });
 const app: Express = express();
@@ -32,9 +45,9 @@ app.use(requestLogger);
 app.use("/health-check", healthCheckRouter);
 app.use("/users", userRouter);
 app.post("/", function(req, res) {
+  const body = req.body as unknown as Payload;
   const logsPath = "logs.json";
   const newData = JSON.stringify(req.body) + "\n";
-
   appendFile(logsPath, newData, (err) => {
     if (err) {
       console.error(err);
