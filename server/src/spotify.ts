01c06f9888d1ad53691f56a6762d2a506153b567
Darth Vader <vader.pepe@email.com>
2025-08-08 18:20:49 +0700

    feat: ketinggalan

--- server/src/spotify.ts
+++ server/src/spotify.ts
@@ -2,15 +2,131 @@ import axios from "axios";
 import { env } from "@/common/utils/envConfig";
 import { spotify } from ".";
 
-export async function getNowPlayingUri(): Promise<string> {
+interface Player {
+  device: Device;
+  repeat_state: string;
+  shuffle_state: boolean;
+  context: Context;
+  timestamp: number;
+  progress_ms: number;
+  is_playing: boolean;
+  item: Item;
+  currently_playing_type: string;
+  actions: Actions;
+};
+
+interface Actions {
+  interrupting_playback: boolean;
+  pausing: boolean;
+  resuming: boolean;
+  seeking: boolean;
+  skipping_next: boolean;
+  skipping_prev: boolean;
+  toggling_repeat_context: boolean;
+  toggling_shuffle: boolean;
+  toggling_repeat_track: boolean;
+  transferring_playback: boolean;
+};
+
+interface Context {
+  type: string;
+  href: string;
+  external_urls: ExternalUrls;
+  uri: string;
+};
+
+interface ExternalUrls {
+  spotify: string;
+};
+
+interface Device {
+  id: string;
+  is_active: boolean;
+  is_private_session: boolean;
+  is_restricted: boolean;
+  name: string;
+  type: string;
+  volume_percent: number;
+  supports_volume: boolean;
+};
+
+interface Item {
+  album: Album;
+  artists: Artist[];
+  available_markets: string[];
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_ids: ExternalIDS;
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  is_playable: boolean;
+  linked_from: LinkedFrom;
+  restrictions: Restrictions;
+  name: string;
+  popularity: number;
+  preview_url: string;
+  track_number: number;
+  type: string;
+  uri: string;
+  is_local: boolean;
+};
+
+interface Album {
+  album_type: string;
+  total_tracks: number;
+  available_markets: string[];
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  images: Image[];
+  name: string;
+  release_date: string;
+  release_date_precision: string;
+  restrictions: Restrictions;
+  type: string;
+  uri: string;
+  artists: Artist[];
+};
+
+interface Artist {
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  name: string;
+  type: string;
+  uri: string;
+};
+
+interface Image {
+  url: string;
+  height: number;
+  width: number;
+};
+
+interface Restrictions {
+  reason: string;
+};
+
+interface ExternalIDS {
+  isrc: string;
+  ean: string;
+  upc: string;
+};
+
+interface LinkedFrom {
+};
+
+export async function getNowPlaying() {
   const token = (await getAccessToken()).access_token;
-  const res = await axios.get('https://api.spotify.com/v1/me/player', {
+  const res = await axios.get<Player>('https://api.spotify.com/v1/me/player', {
     headers: {
       Authorization: `Bearer ${token}`
     }
   });
 
-  return res.data.item.uri as string;
+  return res.data;
 };
 
 interface Response {
@@ -47,4 +163,16 @@ async function getAccessToken(): Promise<Response> {
 
   spotify.setToken({ ...response.data, updated_at: now });
   return response.data;
+};
+
+export async function pollNowPlaying(handleTrack: () => Promise<void>) {
+  const data = await getNowPlaying();
+  if (data.is_playing) {
+    const remaining = data.item.duration_ms - data.progress_ms;
+    // handleTrack(data.item); // your handler
+    await handleTrack();
+    setTimeout(pollNowPlaying, remaining + 1000);
+  } else {
+    setTimeout(pollNowPlaying, 30000);
+  }
 };
